49313ac203cc3548d602aa9c5adbbff195b6f433
borajin <55897222+borajin@users.noreply.github.com>
2023-12-20 20:32:27 +0000

    Deploying to gh-pages from @ warchivee/warchive-web@97955ded096b663d6d427063027e4a2462ff06ca 🚀

---
+++ storybook/assets/caption.stories-AV73Iljh.js
@@ -0,0 +1,5 @@
+import{C as o}from"./index-vemNTFF3.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";const n={component:o,title:"Text/Caption",parameters:{layout:"centered"},tags:["autodocs"]},e={args:{value:"캡션"}};var a,r,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    value: '캡션'
+  }
+}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const c=["Example"];export{e as Example,c as __namedExportsOrder,n as default};
