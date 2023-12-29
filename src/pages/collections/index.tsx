d823e730e00f97d76fc713069611b56671803da1
borajin <borajin.soft@gmail.com>
2023-12-29 15:48:18 +0900

    chor: 키워드 클릭 시 메인 페이지로 이동하며, 이전 카테고리 검색 기록이 남아있는 현상 수정 (#18)

--- src/pages/collections/index.tsx
+++ src/pages/collections/index.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from 'react';
 import useBookmarkList from 'src/hooks/useCollections';
 import WataCardList from '@components/wata/list';
+import { useRecoilState } from 'recoil';
+import { searchKeywordState } from 'src/data/search.atom';
 import CollectionMenu from './components/menu';
 import CollectionTitle from './components/title';
 import ShareCollectionButtons from './components/share';
 
 export default function Collections() {
+  const [searchKeywords, setSearchKeywords] =
+    useRecoilState(searchKeywordState);
   const { collections } = useBookmarkList();
   const [collectionIndex, setCollectionIndex] = useState<number>(0);
   const [editMode, setEditMode] = useState<boolean>(false);
@@ -13,6 +17,18 @@ export default function Collections() {
   useEffect(() => {
     setEditMode(false);
   }, [collectionIndex]);
+
+  // 키워드 클릭 시 메인 페이지로 이동하며, 이전 카테고리 검색 기록이 남아있는 현상 수정
+  useEffect(() => {
+    setSearchKeywords({
+      ...searchKeywords,
+      category: {
+        label: '전체',
+        value: 'category-전체',
+      },
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="page collections">
