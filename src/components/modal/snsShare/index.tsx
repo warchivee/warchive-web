9af3bc1c5d162914607fa333238ee89e915d2608
Kim Jaea <rkddnjs58@gmail.com>
2024-01-08 20:53:58 +0900

    feat: 컬렉션 공유 UI 수정 (#30)

--- src/components/modal/snsShare/index.tsx
+++ src/components/modal/snsShare/index.tsx
@@ -2,66 +2,152 @@ import Button from '@components/button';
 import { getSharedCollectionShortUrl } from '@utils/collections/shareUrlShroter';
 import useCollections from 'src/hooks/useCollections';
 import CopyToClipboard from 'react-copy-to-clipboard';
-import Modal from '..';
-import { ModalProps } from '../index.type';
+import { Text } from '@components/text';
+import classNames from 'classnames';
 
-interface SnsShareModalProps extends ModalProps {
+import kakaotalk_logo from '@assets/logos/kakaotalk.png';
+import twitter_logo from '@assets/logos/twitter.png';
+import instagram_logo from '@assets/logos/instagram.png';
+import facebook_logo from '@assets/logos/facebook.png';
+
+interface SnsShareModalProps {
+  isOpen: boolean;
+  onClose: () => void;
   selectIndex: number;
 }
 
 export default function SnsShareModal({
-  selectIndex,
   isOpen,
   onClose,
+  selectIndex,
 }: SnsShareModalProps) {
   const { collections } = useCollections();
 
   const shareUrl = getSharedCollectionShortUrl(collections[selectIndex]);
 
+  const imageSize = {
+    width: '28px',
+    height: '28px',
+  };
+
+  const showNotification = () => {
+    alert('주소가 복사되었습니다.');
+  };
+
   return (
-    <Modal
-      isOpen={isOpen}
-      onClose={onClose}
-      title="SNS에 공유하기"
-      message="SNS에 내 컬렉션을 공유해보세요."
-    >
-      <div className="buttons">
-        <CopyToClipboard text={shareUrl} onCopy={() => onClose()}>
-          <Button
-            icon="link"
-            background="selago"
-            iconColor="purple"
-            labelColor="ebony"
-            border="round"
+    <div className={classNames('sns-share-modal', { close: !isOpen })}>
+      <div className="arrow" />
+      <div className="inner">
+        <div className="header">
+          <div className="title">
+            <Text size="big">공유하기</Text>
+          </div>
+          <Button icon="xmark" iconColor="black" size="big" onClick={onClose} />
+        </div>
+
+        <div className="body">
+          <button className="share-group">
+            <img
+              src={kakaotalk_logo}
+              alt="카카오톡으로 공유하기"
+              style={imageSize}
+            />
+            <Text size="small">카카오톡</Text>
+          </button>
+
+          <button
+            className="share-group"
+            onClick={() => {
+              // 트위터 본문 내용 : 280자 제한
+              const sendText = ' ';
+              window.open(
+                `https://twitter.com/intent/tweet?text=${sendText}&url=${shareUrl}`,
+              );
+            }}
           >
-            공유 URL 복사
-          </Button>
-        </CopyToClipboard>
-        <Button
-          icon="twitter"
-          background="selago"
-          labelColor="ebony"
-          border="round"
-          onClick={() => {
-            // 트위터 본문 내용 : 280자 제한
-            const sendText = ' ';
-            window.open(
-              `https://twitter.com/intent/tweet?text=${sendText}&url=${shareUrl}`,
-            );
-          }}
-        />
-        <Button
-          icon="facebook"
-          background="selago"
-          labelColor="ebony"
-          border="round"
-          onClick={() => {
-            window.open(
-              `http://www.facebook.com/sharer/sharer.php?u=${shareUrl}`,
-            );
-          }}
-        />
+            <img src={twitter_logo} alt="트위터로 공유하기" style={imageSize} />
+            <Text size="small">트위터</Text>
+          </button>
+
+          <button className="share-group">
+            <img
+              src={instagram_logo}
+              alt="인스타그램으로 공유하기"
+              style={imageSize}
+            />
+            <Text size="small">인스타그램</Text>
+          </button>
+
+          <button
+            className="share-group"
+            onClick={() => {
+              window.open(
+                `http://www.facebook.com/sharer/sharer.php?u=${shareUrl}`,
+              );
+            }}
+          >
+            <img
+              src={facebook_logo}
+              alt="페이스북으로 공유하기"
+              style={imageSize}
+            />
+            <Text size="small">페이스북</Text>
+          </button>
+
+          <CopyToClipboard text={shareUrl} onCopy={() => showNotification()}>
+            <button className="share-group">
+              <Button
+                background="light-gray"
+                border="round"
+                icon="link"
+                iconColor="black"
+              />
+              <Text size="small">링크복사</Text>
+            </button>
+          </CopyToClipboard>
+        </div>
       </div>
-    </Modal>
+    </div>
+
+    // <Modal isOpen={isOpen} onClose={onClose} title="공유하기">
+    //   <div className="buttons">
+    //
+    //     {/* <CopyToClipboard text={shareUrl} onCopy={() => onClose()}>
+    //       <Button
+    //         icon="link"
+    //         background="selago"
+    //         iconColor="purple"
+    //         labelColor="ebony"
+    //         border="round"
+    //       >
+    //         공유 URL 복사
+    //       </Button>
+    //     </CopyToClipboard> */}
+    //     {/* <Button
+    //       icon="twitter"
+    //       background="selago"
+    //       labelColor="ebony"
+    //       border="round"
+    //       onClick={() => {
+    //         // 트위터 본문 내용 : 280자 제한
+    //         const sendText = ' ';
+    //         window.open(
+    //           `https://twitter.com/intent/tweet?text=${sendText}&url=${shareUrl}`,
+    //         );
+    //       }}
+    //     /> */}
+    //     {/* <Button
+    //       icon="facebook"
+    //       background="selago"
+    //       labelColor="ebony"
+    //       border="round"
+    //       onClick={() => {
+    //         window.open(
+    //           `http://www.facebook.com/sharer/sharer.php?u=${shareUrl}`,
+    //         );
+    //       }}
+    //     /> */}
+    //   </div>
+    // </Modal>
   );
 }
