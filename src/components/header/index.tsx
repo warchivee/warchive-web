a9cf4edb54e034f2bf3860f9642d40145256a079
Kim Jaea <rkddnjs58@gmail.com>
2024-01-21 09:18:34 +0900

    feat: 햄버거바 선택 시 보이는 사이드 메뉴 완성 (#32)

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,6 @@
 import { Link, useLocation } from 'react-router-dom';
 import React, { useState } from 'react';
 import Button from '@components/button';
-import Footer from '@components/footer';
 import { Text, Title } from '@components/text';
 import { HeaderProps, MenuInfo } from './index.type';
 
@@ -41,6 +40,12 @@ export default function Header({
   const handleHamburgereMenu = () => {
     setMenuOpen(!isMenuOpen);
   };
+  const openReportModal = () => {
+    window.open(
+      'https://docs.google.com/forms/d/e/1FAIpQLSfvn7m8JTfXCt57EkJLkXo66a6FB2ra0hzN9PE4CyVNZcuzHg/viewform',
+      '_blank',
+    );
+  };
 
   return (
     <>
@@ -62,7 +67,6 @@ export default function Header({
 
       <div className="mobile-header-background">
         <header>
-          {/* 햄버거 바 버튼 */}
           <div className="menu-hamburger">
             <Button
               icon="hambuger-menu"
@@ -85,35 +89,106 @@ export default function Header({
       </div>
 
       <div className={`mobile-hamburger-side ${isMenuOpen ? 'open' : ''}`}>
-        <div className="hamburger-header">
-          <div className="close-button">
-            <Button
-              icon="xmark"
-              iconColor="white"
-              onClick={handleHamburgereMenu}
-            />
-          </div>
-          <div className="info">
-            <div className="logo">
-              <img src="/images/logo/logo.png" alt="와카이브 로고" />
+        <div className="hamburger-container">
+          <div className="hamburger-header">
+            <div className="close-button">
+              <Button
+                icon="xmark"
+                iconColor="white"
+                size="big"
+                onClick={handleHamburgereMenu}
+              />
             </div>
-            <div className="text">
-              <Text size="small" color="white">
-                여성서사 아카이브 프로젝트
-              </Text>
-              <Title type="h2" color="white">
-                Warchive
-              </Title>
+            <div className="info">
+              <div className="logo">
+                <img src="/images/logo/logo.png" alt="와카이브 로고" />
+              </div>
+              <div className="text">
+                <Text size="small" color="white">
+                  여성서사 아카이브 프로젝트
+                </Text>
+                <Title type="h2" color="white">
+                  Warchive
+                </Title>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="hamburger-menus">
-          {/* 컬렉션 */}
-          {/* 추천작 제보/문의 */}
-          {/* 와카이브 소개 */}
-        </div>
-        <div className="hamburger-footer">
-          <Footer />
+          <div className="hamburger-menus">
+            <div className="menus">
+              <Link to="/collections">
+                <Button
+                  icon="star"
+                  iconColor="white"
+                  labelColor="white"
+                  size="big"
+                  onClick={handleHamburgereMenu}
+                >
+                  북마크 목록
+                </Button>
+              </Link>
+              <Button
+                icon="mail"
+                iconColor="white"
+                labelColor="white"
+                size="big"
+                onClick={() => {
+                  openReportModal();
+                  handleHamburgereMenu();
+                }}
+              >
+                추천작 제보/문의
+              </Button>
+              <Link to="/about">
+                <Button
+                  icon="question"
+                  iconColor="white"
+                  labelColor="white"
+                  size="big"
+                  onClick={handleHamburgereMenu}
+                >
+                  와카이브 소개
+                </Button>
+              </Link>
+            </div>
+          </div>
+          <div className="hamburger-footer">
+            <footer>
+              <div className="connect">
+                <a
+                  href="https://twitter.com/Womynarchive"
+                  target="_blank"
+                  aria-label="트위터로 이동"
+                  rel="noreferrer"
+                >
+                  <Button
+                    icon="twitter"
+                    iconColor="ebony"
+                    background="twitter-blue"
+                  />
+                </a>
+              </div>
+              <div className="bar" />
+              <div className="info">
+                <div>
+                  <Text color="gray" size="small">
+                    연락처
+                  </Text>
+                  <Text color="gray" size="small">
+                    team.warchive@gmail.com
+                  </Text>
+                </div>
+
+                <div>
+                  <Text color="gray" size="small">
+                    후원계좌
+                  </Text>
+                  <Text color="gray" size="small">
+                    우리 1002 343 024735 ㅇㅈㅇ
+                  </Text>
+                </div>
+              </div>
+            </footer>
+          </div>
         </div>
       </div>
     </>
