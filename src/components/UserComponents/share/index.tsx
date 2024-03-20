5d5d303051667769f660a6a16f2342f542263c38
Kim Jaea <rkddnjs58@gmail.com>
2024-03-20 14:06:55 +0900

    fix: CollectionShareButtonsProps 에러 해결

--- src/components/UserComponents/share/index.tsx
+++ src/components/UserComponents/share/index.tsx
@@ -5,30 +5,25 @@ import { DEFAULT_COLLECTIONS_KEY } from 'src/types/collection.type';
 import { useState } from 'react';
 
 export default function ShareCollectionButtons({
-  isEditMode,
   selectIndex = 0,
 }: CollectionShareButtonsProps) {
   const [openSnsShareModal, setOpenSnsShareModal] = useState<boolean>(false);
   return (
     !(selectIndex === DEFAULT_COLLECTIONS_KEY) && (
       <div className="sns">
-        {!isEditMode && (
-          <>
-            <Button
-              icon="share"
-              iconColor="black"
-              background="light-gray"
-              onClick={() => setOpenSnsShareModal(true)}
-            >
-              나만 보기 아까운 작품이 있다면?
-            </Button>
-            <SnsShareModal
-              isOpen={openSnsShareModal}
-              onClose={() => setOpenSnsShareModal(false)}
-              selectIndex={selectIndex}
-            />
-          </>
-        )}
+        <Button
+          icon="share"
+          iconColor="black"
+          background="light-gray"
+          onClick={() => setOpenSnsShareModal(true)}
+        >
+          나만 보기 아까운 작품이 있다면?
+        </Button>
+        <SnsShareModal
+          isOpen={openSnsShareModal}
+          onClose={() => setOpenSnsShareModal(false)}
+          selectIndex={selectIndex}
+        />
       </div>
     )
   );
