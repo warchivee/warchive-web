9bf9a6d2e87a0821045521124f5158da1e397b45
borajin <borajin.soft@gmail.com>
2024-04-06 02:54:01 +0900

    🐛 fix: vercel 배포 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,4 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths(), vercel()],
   cacheDir: '.vite/',
   base: '', // github page 로 배포 시 /warchive-web/ (레포명) 을 여기에 입력하고, Browser Router 의 baseUrl 에도 입력합니다.
-  build: {
-    outDir: 'build',
-  },
 });
