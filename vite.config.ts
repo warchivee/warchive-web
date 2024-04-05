d91528bd07175f2d6881816ec45b442b495df56f
borajin <borajin.soft@gmail.com>
2024-04-06 04:21:14 +0900

    🐛 fix: vercel 배포 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import vercel from 'vite-plugin-vercel';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), vercel()],
+  plugins: [react(), tsconfigPaths(), svgr()],
   cacheDir: '.vite/',
   base: '', // github page 로 배포 시 /warchive-web/ (레포명) 을 여기에 입력하고, Browser Router 의 baseUrl 에도 입력합니다.
-  vercel: {
+  build: {
     outDir: 'build',
   },
 });
