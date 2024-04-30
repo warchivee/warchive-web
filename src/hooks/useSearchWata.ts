d4ffda3c12ce8d1106ae60cab950da2b6fca5649
borajin <borajin.soft@gmail.com>
2024-05-01 01:18:54 +0900

    🩹 chor: 데이터 임시로 정렬 뒤집기 (캡처용)

--- src/hooks/useSearchWata.ts
+++ src/hooks/useSearchWata.ts
@@ -28,53 +28,55 @@ const useSearchWata = () => {
   };
 
   const getSearchWatas = async () => {
-    const filterWatas = watas?.filter((wata: WataType) => {
-      const pass = {
-        searchInput: true,
-        category: true,
-        genres: true,
-        platforms: true,
-        keywords: true,
-      };
+    const filterWatas = watas
+      ?.filter((wata: WataType) => {
+        const pass = {
+          searchInput: true,
+          category: true,
+          genres: true,
+          platforms: true,
+          keywords: true,
+        };
 
-      if (searchInput && searchInput?.replace(' ', '') !== '') {
-        pass.searchInput =
-          wata.title
-            ?.replace(' ', '')
-            ?.toLocaleLowerCase()
-            .includes(searchInput?.replace(' ', '')?.toLocaleLowerCase()) ||
-          wata.creators
-            ?.replace(' ', '')
-            ?.toLocaleLowerCase()
-            .includes(searchInput?.replace(' ', '')?.toLocaleLowerCase());
-      }
+        if (searchInput && searchInput?.replace(' ', '') !== '') {
+          pass.searchInput =
+            wata.title
+              ?.replace(' ', '')
+              ?.toLocaleLowerCase()
+              .includes(searchInput?.replace(' ', '')?.toLocaleLowerCase()) ||
+            wata.creators
+              ?.replace(' ', '')
+              ?.toLocaleLowerCase()
+              .includes(searchInput?.replace(' ', '')?.toLocaleLowerCase());
+        }
 
-      if (category && category.id !== 0) {
-        pass.category = wata.category.id === +category.id;
-      }
+        if (category && category.id !== 0) {
+          pass.category = wata.category.id === +category.id;
+        }
 
-      if (genres && genres.length !== 0) {
-        pass.genres = genres?.some((g) => wata.genre.id === g.id);
-      }
+        if (genres && genres.length !== 0) {
+          pass.genres = genres?.some((g) => wata.genre.id === g.id);
+        }
 
-      if (platforms && platforms.length !== 0) {
-        const platformIds = new Set(platforms?.map((p) => p.id));
-        pass.platforms = wata.platforms?.some((wp) => platformIds.has(wp.id));
-      }
+        if (platforms && platforms.length !== 0) {
+          const platformIds = new Set(platforms?.map((p) => p.id));
+          pass.platforms = wata.platforms?.some((wp) => platformIds.has(wp.id));
+        }
 
-      if (keywords && keywords.length !== 0) {
-        const keywordIds = new Set(keywords?.map((p) => p.id));
-        pass.keywords = wata.keywords?.some((wp) => keywordIds.has(wp.id));
-      }
+        if (keywords && keywords.length !== 0) {
+          const keywordIds = new Set(keywords?.map((p) => p.id));
+          pass.keywords = wata.keywords?.some((wp) => keywordIds.has(wp.id));
+        }
 
-      return (
-        pass.searchInput &&
-        pass.category &&
-        pass.genres &&
-        pass.platforms &&
-        pass.keywords
-      );
-    });
+        return (
+          pass.searchInput &&
+          pass.category &&
+          pass.genres &&
+          pass.platforms &&
+          pass.keywords
+        );
+      })
+      .reverse();
 
     setSearchAllwata(filterWatas);
     pageSearchWata(filterWatas);
