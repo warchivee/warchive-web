2c5cbb8b12417572437d8676c8c47979446234c3
borajin <borajin.soft@gmail.com>
2024-04-30 20:02:25 +0900

    🩹 chor: 스크랩북 개수 제한 변경

--- src/pages/Scrapbooks.tsx
+++ src/pages/Scrapbooks.tsx
@@ -21,6 +21,10 @@ import {
   faFaceSurprise,
   faGrinWink,
 } from '@fortawesome/free-regular-svg-icons';
+import {
+  SCRAPBOOKS_LIMMIT_COUNT,
+  SCRAPBOOK_COMMENT_LIMIT_LENGTH,
+} from '@utils/consts/scrapbooks.const';
 
 export default function Scrapbooks() {
   const { refreshScrapbookState, getScrapbookItems, isScrapbooksEmpty } =
@@ -73,7 +77,7 @@ export default function Scrapbooks() {
                 icon={faFaceSurprise}
                 title="스크랩북이 없어요"
                 content={
-                  <Typography>
+                  <Typography textAlign="center">
                     스크랩북 목록의{' '}
                     <IconButton
                       size="sm"
@@ -86,6 +90,12 @@ export default function Scrapbooks() {
                       <FontAwesomeIcon icon={faPlus} />
                     </IconButton>{' '}
                     버튼을 눌러 내 여성서사 스크랩북을 만들어보세요.
+                    <br />
+                    스크랩북은{' '}
+                    <Typography sx={{ textDecoration: 'underline' }}>
+                      {SCRAPBOOKS_LIMMIT_COUNT}개
+                    </Typography>
+                    까지 만들 수 있습니다.
                   </Typography>
                 }
               />
@@ -97,12 +107,18 @@ export default function Scrapbooks() {
                     icon={faGrinWink}
                     title="좋아하는 작품을 추가해보세요"
                     content={
-                      <Typography>
+                      <Typography textAlign="center">
                         원하는 작품 카드의
                         <IconButton sx={{ cursor: 'text' }}>
                           <FontAwesomeIcon icon={faBookBookmark} />
                         </IconButton>
-                        버튼을 눌러 내 스크랩북에 추가할 수 있습니다.
+                        버튼을 눌러 내 스크랩북에 추가해보세요.
+                        <br />
+                        스크랩북에는{' '}
+                        <Typography sx={{ textDecoration: 'underline' }}>
+                          {SCRAPBOOK_COMMENT_LIMIT_LENGTH}개
+                        </Typography>
+                        의 작품을 추가할 수 있습니다.
                       </Typography>
                     }
                   />
